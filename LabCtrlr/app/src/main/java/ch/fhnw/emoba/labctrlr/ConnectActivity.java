408ddfd1c275e28f81ae44dcd97d80621f4fc92c
lukasmusy <lukasmusy@gmail.com>
2016-05-03 15:55:11 +0200

    connect view

--- LabCtrlr/app/src/main/java/ch/fhnw/emoba/labctrlr/ConnectActivity.java
+++ LabCtrlr/app/src/main/java/ch/fhnw/emoba/labctrlr/ConnectActivity.java
@@ -1,7 +1,10 @@
 package ch.fhnw.emoba.labctrlr;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ConnectActivity extends AppCompatActivity {
 
@@ -10,4 +13,19 @@ public class ConnectActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect);
     }
+
+    public void onConnectClick(View v) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void onConnectionError() {
+        //TODO take argument and adjust status message correspondingly
+        TextView statusText = (TextView) findViewById(R.id.textView_status);
+        statusText.setText("Failed to connect to "+ "[IP]:[Port]");
+
+    }
+
+
+
 }
